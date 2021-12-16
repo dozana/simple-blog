1d30b877e813d03338be1dbee7f7fb268bda0f31
Dozana <levan.soroznishvili@gmail.com>
2021-12-17 01:05:34 +0400

    Tag selector

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -52,7 +52,7 @@
                 @if($tags->count() > 0)
                     <div class="form-group">
                         <label for="tags">Tags</label>
-                        <select name="tags[]" id="tags" class="form-control" multiple>
+                        <select name="tags[]" id="tags" class="form-control tags-selector" multiple>
                             @foreach($tags as $tag)
                                 <option value="{{ $tag->id }}">
                                     {{ $tag->title }}
@@ -72,14 +72,20 @@
 @section('styles')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css"/>
 @endsection
 
 @section('scripts')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script>
         flatpickr("#published_at", {
             enableTime: true
+        });
+
+        $(document).ready(function() {
+            $('.tags-selector').select2();
         });
     </script>
 @endsection
